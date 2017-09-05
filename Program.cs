0a71357755b651400c63345a9d03a30f072eea8b
Sujith Nair <sujith.nair@thomsonreuters.com>
2017-09-04 23:30:30 -0400

    Permutate string

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace CrackingCode
     {
         static void Main(string[] args)
         {
+            Mystring.permutation("abcd");
         }
     }
 }
---
+++ String/MyString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingCode
+{
+    static class Mystring
+    {
+        public static void permutation(string str)
+        {
+            permutation(str, "");
+        }
+
+        private static void permutation(string str, string prefix)
+        {
+            if (str.Length == 0)
+            {
+                Console.WriteLine(prefix);
+            }
+            else
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    string rem = str.Substring(0, i) + str.Substring(i + 1);
+                    permutation(rem, prefix + str[i]);
+                }
+            }
+        }        
+    }
+}
